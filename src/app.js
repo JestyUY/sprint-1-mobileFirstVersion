d7d06cbcc198c327818d83d41492530b5940001e
JestyUY <104555840+JestyUY@users.noreply.github.com>
2023-06-22 09:35:16 +1200

    some functionality has been added to inputs

--- src/app.js
+++ src/app.js
@@ -8,8 +8,6 @@ nombreTarjeta.addEventListener("input", () =>{
 })
 
 
-
-
 let cardNumberInp = document.getElementById('cardnumber')
 let cardNumberDisp = document.getElementById('cardNumberDisp')
 
@@ -17,3 +15,26 @@ cardNumberInp.addEventListener("input", () => {
   const inpValue = cardNumberInp.value;
   cardNumberDisp.innerText = inpValue;
 })
+
+let cvcInp = document.getElementById('cvc-input') 
+let cvc = document.getElementById('cvc')
+
+cvcInp.addEventListener('input', ()=>  {
+  const cvcTemp = cvcInp.value;
+  cvc.innerText = cvcTemp
+})
+
+let expDate = document.getElementById('expDate')
+let monthInp = document.getElementById('month-input')
+let yearInp = document.getElementById('year-input')
+
+monthInp.addEventListener('input',()=>{
+const expDateFinal = monthInp.value + "/" + yearInp.value
+expDate.innerText = expDateFinal
+})
+yearInp.addEventListener('input',()=>{
+const expDateFinal = monthInp.value + "/" + yearInp.value
+expDate.innerText = expDateFinal
+})
+
+
